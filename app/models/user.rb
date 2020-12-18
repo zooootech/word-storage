d36552a5a0062ccf316b0bde698b8e888d8c078a
zooootech <yzmisng@gmail.com>
2020-12-18 15:51:03 +0900

    Userモデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,33 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:name] # 認証のキーをメールから名前へ
+
+  validates_uniqueness_of :name
+  validates_presence_of :name
+
+  # nameを利用してログイン（ユーザー名で検索）
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["name = :value", { :value => name }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  # 登録時に email を不要にする
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  # 「Undefined method will_save_change_to_email?」エラーを防ぐ為に以下を記述
+  def will_save_change_to_email?
+    false
+  end
 end

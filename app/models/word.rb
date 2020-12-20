233242e3178a9df520aeb75c08b048d6868f4df1
zooootech <yzmisng@gmail.com>
2020-12-20 19:52:17 +0900

    Wordモデルのアソシエーションとバリデーションを設定

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,12 @@
 class Word < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :english
+    validates :japanese
+  end
+
+  validates :english, format: {
+    with:  /\A[a-zA-Z]+\z/, message: 'には半角アルファベットを使用してください'
+  }, allow_blank: true
 end

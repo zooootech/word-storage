cfe1f0b47e9cc954d270bd26dc00d02549f7dd3e
zooootech <yzmisng@gmail.com>
2020-12-20 22:02:17 +0900

    ワード投稿のアクションとストロングパラメーターを定義

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,25 @@ class WordsController < ApplicationController
   def index
     @words = Word.all.order(:english)
   end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    if @word.valid?
+      @word.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def word_params
+    params.require(:word).permit(:english, :japanese, :remarks).merge(user_id: current_user.id)
+  end
+
+
 end

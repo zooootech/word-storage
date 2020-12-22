d5cd2d54c7434281972dec86673aa5d5a9ebcc83
zooootech <yzmisng@gmail.com>
2020-12-22 23:57:03 +0900

    詳細ページのルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'words#index'
-  resources :words, only: [:index, :new, :create]
+  resources :words, only: [:index, :new, :create, :show]
 end

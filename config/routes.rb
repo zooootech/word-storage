c76258e2717dd26836b46e39c3f195a11626d026
zooootech <yzmisng@gmail.com>
2020-12-27 20:16:30 +0900

    編集機能のルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'words#index'
-  resources :words, only: [:index, :new, :create, :show, :destroy]
+  resources :words, only: [:index, :new, :create, :show, :destroy, :edit]
 end

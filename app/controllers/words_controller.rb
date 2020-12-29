38ec1a2a3deda6e1aa5961c9e8dd6aa4eb3be0f3
zooootech <yzmisng@gmail.com>
2020-12-29 17:03:43 +0900

    updateアクションの定義

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -33,6 +33,15 @@ class WordsController < ApplicationController
     redirect_to word_path(@word) if current_user != @word.user
   end
 
+  def update
+    @word = Word.find(params[:id])
+    if @word.update(word_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def word_params
     params.require(:word).permit(:english, :japanese, :remarks).merge(user_id: current_user.id)

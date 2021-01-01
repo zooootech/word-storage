7447f2eee7d1b106097b46aeb226891a3379dcb5
zooootech <yzmisng@gmail.com>
2021-01-02 00:25:56 +0900

    お気に入り機能のルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'words#index'
-  resources :words
+  resources :words do
+    resources :favorites, only: [:create, :destroy]
+  end
 end

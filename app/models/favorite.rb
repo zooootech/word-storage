8db1b7196c1d608abf0a4bef76ebfa4eb260452f
zooootech <yzmisng@gmail.com>
2021-01-01 22:10:45 +0900

    Favoriteモデルのアソシエーションとバリデーションの設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,8 @@
 class Favorite < ApplicationRecord
+  belongs_to :user
+  belongs_to :word
+
+  # uniquenessヘルパーを用いて、モデルレベルで一意性をチェック
+  # scopeオプションで、一意性チェックの範囲を限定する属性を指定
+  validates :user_id, uniqueness: { scope: :word_id }
 end

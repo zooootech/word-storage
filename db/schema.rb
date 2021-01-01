a89d3f1a32c04e71b53f2ba04cff582c85828a3c
zooootech <yzmisng@gmail.com>
2021-01-01 21:07:44 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_071516) do
+ActiveRecord::Schema.define(version: 2020_12_30_093038) do
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "word_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+    t.index ["word_id"], name: "index_favorites_on_word_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -34,5 +43,7 @@ ActiveRecord::Schema.define(version: 2020_12_18_071516) do
     t.index ["user_id"], name: "index_words_on_user_id"
   end
 
+  add_foreign_key "favorites", "users"
+  add_foreign_key "favorites", "words"
   add_foreign_key "words", "users"
 end

53f95fbdd9bd3c8746abd9c2ddf5965b2d77b93e
zooootech <yzmisng@gmail.com>
2021-01-04 23:25:27 +0900

    Revert "favoritesコントローラーの作成"
    This reverts commit 0493ec239f08ec85d8969c9ef47434867a893ca9.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,2 @@
 class FavoritesController < ApplicationController
-  def create
-    @favorite = Favorite.create(like_params)
-  end
-
-  private
-
-  def like_params
-    @word = Word.find(params[:id])
-    params.require(:favorite).merge(user_id: current_user.id, word_id: @word.id)
-  end
-
 end

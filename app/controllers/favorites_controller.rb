ef126f730825d40d42a7369163f6895f5e569a54
zooootech <yzmisng@gmail.com>
2021-01-08 23:12:53 +0900

    favoritesコントローラーの修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = Favorite.create(like_params)
+    @word = Word.find(params[:word_id])
+    @favorite = Favorite.create(user_id: current_user.id, word_id: @word.id)
+    redirect_to root_path
   end
 
   def destroy
     @word = Word.find(params[:id])
     @favorite = Favorite.find_by(user_id: current_user.id, word_id: @word.id)
     @favorite.destroy
+    redirect_to root_path
   end
-
-  private
-
-  def like_params
-    @word = Word.find(params[:id])
-    params.require(:favorite).merge(user_id: current_user.id, word_id: @word.id)
-  end
-
 end

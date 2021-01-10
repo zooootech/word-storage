ca851651a32c3ca7d9da0fce10df8629de1df104
zooootech <yzmisng@gmail.com>
2021-01-10 18:53:33 +0900

    Wordモデルの修正

--- app/models/word.rb
+++ app/models/word.rb
@@ -14,12 +14,28 @@ class Word < ApplicationRecord
 
   # データに対する処理などを行うプログラム処理（＝ビジネスロジック）は、モデルに置く
   # クラスメソッド（クラスで共通の情報を使った処理）は、メソッド名の前にselfを.(ドット)で繋げて定義
-  def self.search(search)
+  def self.search_english(search)
     # 検索フォームに何か値が入力されていた場合
     if search != ""
-      Word.where('english LIKE(?)', "%#{search}%").where('japanese LIKE(?)', "%#{search}%").where('remarks LIKE(?)', "%#{search}%")
+      Word.where('english LIKE(?)', "%#{search}%")
     else
-      Word.all
+      Word.all.order(:english)
+    end
+  end
+
+  def self.search_japanese(search)
+    if search != ""
+      Word.where('japanese LIKE(?)', "%#{search}%")
+    else
+      Word.all.order(:english)
+    end
+  end
+
+  def self.search_remarks(search)
+    if search != ""
+      Word.where('remarks LIKE(?)', "%#{search}%")
+    else
+      Word.all.order(:english)
     end
   end
 end

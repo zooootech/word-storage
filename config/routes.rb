28cb882d7dfa6a2626a8b79fecf280b1b8dd337b
zooootech <yzmisng@gmail.com>
2021-01-10 14:51:09 +0900

    検索機能のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'words#index'
   resources :words do
     resources :favorites, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end

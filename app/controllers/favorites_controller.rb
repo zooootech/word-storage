1439164b236161bc39e53b9c162af0d4bc86cfea
zooootech <yzmisng@gmail.com>
2021-01-12 21:06:44 +0900

    favoritesコントローラーの編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_word
+  
   def create
-    @word = Word.find(params[:word_id])
     @favorite = Favorite.create(user_id: current_user.id, word_id: @word.id)
     redirect_to user_path(current_user)
   end
 
   def destroy
-    @word = Word.find(params[:id])
     @favorite = Favorite.find_by(user_id: current_user.id, word_id: @word.id)
     @favorite.destroy
     redirect_to user_path(current_user)
+  end
+
+  private
+  def set_word
+    @word = Word.find(params[:word_id])
   end
 end

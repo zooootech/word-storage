dc5311316229309479c486b04786d884621005c1
zooootech <yzmisng@gmail.com>
2021-01-15 23:27:40 +0900

    UserモデルのFactoryBotの編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name                  { Faker::Name.name }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
   end
 end

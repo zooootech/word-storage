14a3487dd07b31ecb93a5591ad8ad197cbeda73f
zooootech <yzmisng@gmail.com>
2021-01-15 23:32:19 +0900

    Userモデルの単体テストコードを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context 'ユーザー新規登録がうまくいくとき' do
+      it 'nameとpasswordが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passeordが6文字以上であれば登録できる' do
+        @user.password = "123456"
+        @user.password_confirmation = "123456"
+        expect(@user).to be_valid
+      end
+    end
+    context 'ユーザー新規登録がうまくいかないとき' do
+      it 'nameが空では登録できない' do
+        @user.name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordが5文字以下だと登録できない' do
+        @user.password = "12345"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'passwordが存在しても、password_confirmationが空だと登録できない' do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
 end

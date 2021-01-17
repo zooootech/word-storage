8138c34e918181a85b19a7a3fd583a2124c928e9
zooootech <yzmisng@gmail.com>
2021-01-17 18:47:52 +0900

    wordsコントローラーの単体テストコードを記述

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -1,10 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Words", type: :request do
-  describe "GET /words" do
-    it "works! (now write some real specs)" do
-      get words_index_path
-      expect(response).to have_http_status(200)
+  before do
+    @user = FactoryBot.create(:user)
+    @word = FactoryBot.create(:word)
+  end
+
+  describe "GET #index" do
+    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
+      get root_path
+      expect(response.status).to eq(200)
+    end
+    it "indexアクションにリクエストするとレスポンスに登録済みのワードの英語が存在する" do
+      get root_path
+      expect(response.body).to include(@word.english)
+    end
+    it "indexアクションにリクエストするとレスポンスに検索フォームが存在する" do
+      get root_path
+      expect(response.body).to include("検索")
+    end
+  end
+
+  describe "GET #show" do
+    it "showアクションにリクエストすると正常にレスポンスが返ってくる" do
+      sign_in(@user)
+      get word_path(@word)
+      expect(response.status).to eq(200)
+    end
+    it "showアクションにリクエストするとレスポンスに登録済みのワードの英語が存在する" do
+      sign_in(@user)
+      get word_path(@word)
+      expect(response.body).to include(@word.english)
+    end
+    it "showアクションにリクエストするとレスポンスに登録済みにワードの日本語が存在する" do
+      sign_in(@user)
+      get word_path(@word)
+      expect(response.body).to include(@word.japanese)
+    end
+    it "showアクションにリクエストするとレスポンスに登録済みのワードの備考が存在する" do
+      sign_in(@user)
+      get word_path(@word)
+      expect(response.body).to include(@word.remarks)
+    end
+    it "showアクションにリクエストするとレスポンスに検索フォームが存在する" do
+      sign_in(@user)
+      get word_path(@word)
+      expect(response.body).to include("検索")
     end
   end
 end

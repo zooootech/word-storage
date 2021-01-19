fafb072a441bacaad63860d32c24a2aadc03e0a8
zooootech <yzmisng@gmail.com>
2021-01-19 15:48:37 +0900

    WordモデルのFactoryBotの編集

--- spec/factories/words.rb
+++ spec/factories/words.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :word do
-    english  { "apple" }
+    english  { Faker::Lorem.word }
     japanese { Faker::Lorem.sentence }
     remarks  { Faker::Lorem.sentence }
     association :user

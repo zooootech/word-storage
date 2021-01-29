31f9b5040fb7d17fd2d4ed9e62ef7d386895ace7
zooootech <yzmisng@gmail.com>
2021-01-29 12:58:36 +0900

    「ユーザーのお気に入りワードをアルファベット順に並べ替えて取得」のビジネスロジックをモデルに記述
    コントローラーに記述してあるビジネスロジックをモデルに移し、コントローラーでそれを呼び出すように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,6 @@ class UsersController < ApplicationController
   def show
     @name = current_user.name
     @words = current_user.words.order(:english)
-    favorite_words()
-  end
-
-  def favorite_words
-    # 空の配列を作成
-    @favorite_words = []
-    # 現在ログインしているユーザーのお気に入りを変数に代入
-    @favorites = current_user.favorites
-    # 配列型式で格納されているお気に入り情報に対して繰り返し処理を行う
-    @favorites.each do |favorite|
-      # 繰り返し処理の中で、お気に入り情報の内、ワード情報のみを取り出し、配列の要素として追加
-      @favorite_words << favorite.word
-    end
-    # 配列にsort_byメソッドを用いて、昇順（今回の場合、アルファベット順）に並び替え
-    # 破壊的メソッド（!）を使用し、レシーバにあたるオブジェクト（今回の場合、@favorite_words）自身の値を変更
-    @favorite_words.sort_by! do |a|
-      a[:english]
-    end
+    @favorite_words = Favorite.favorite_words(current_user)
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,4 +5,23 @@ class Favorite < ApplicationRecord
   # uniquenessヘルパーを用いて、モデルレベルで一意性をチェック
   # scopeオプションで、一意性チェックの範囲を限定する属性を指定
   validates :user_id, uniqueness: { scope: :word_id }
+
+  # ユーザーのお気に入りにしてあるワードをアルファベット順に並べ替える処理
+  # 引数にはuser_controller.rbにて、current_userが渡される
+  def self.favorite_words(user)
+    # 空の配列を作成
+    favorite_words = []
+    # 現在ログインしているユーザーのお気に入りを変数に代入
+    favorites = user.favorites
+    # 配列型式で格納されているお気に入り情報に対して繰り返し処理を行う
+    favorites.each do |favorite|
+      # 繰り返し処理の中で、お気に入り情報の内、ワード情報のみを取り出し、配列の要素として追加
+      favorite_words << favorite.word
+    end
+    # 配列にsort_byメソッドを用いて、昇順（今回の場合、アルファベット順）に並び替え
+    # 破壊的メソッド（!）を使用し、レシーバにあたるオブジェクト（今回の場合、@favorite_words）自身の値を変更
+    favorite_words.sort_by! do |a|
+      a[:english]
+    end
+  end
 end

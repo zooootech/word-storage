991a3bd1e705b92614aa9e8fbcf8fadcdee6e7b5
zooootech <yzmisng@gmail.com>
2021-01-30 17:06:00 +0900

    Revert "favoritesコントローラーにストロングパラメーターを設定"
    This reverts commit 3fd33ec39d57f20bbb4490eb154e84955d04d466.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   before_action :set_word
   
   def create
-    @favorite = Favorite.create(favorite_params))
+    @favorite = Favorite.create(user_id: current_user.id, word_id: @word.id)
     redirect_to user_path(current_user)
   end
 
@@ -15,9 +15,5 @@ class FavoritesController < ApplicationController
   private
   def set_word
     @word = Word.find(params[:word_id])
-  end
-
-  def favorite_params
-    params.require(:favorite).permit(user_id: current_user.id, word_id: @word.id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @name = current_user.name
     @words = current_user.words.order(:english)
-    # Favoriteモデルに定義されているメソッドを呼び出し、変数に代入
     @favorite_words = Favorite.favorite_words(current_user)
   end
 end

a47236bb30623bb31838aad8715e0cd023c6328d
zooootech <yzmisng@gmail.com>
2021-04-21 00:03:52 +0900

    ログイン中のユーザーのお気に入りワードの取得、並べ替えのコードを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   def show
     @name = current_user.name
     @words = current_user.words.order(:english)
+
     # Favoriteモデルに定義されているメソッドを呼び出し、変数に代入
-    @favorite_words = Favorite.favorite_words(current_user)
+    # @favorite_words = Favorite.favorite_words(current_user)
+
+    @favorite_words = current_user.fav_words.sort_by { |a| [a[:english].downcase, a[:english]] }
   end
 end

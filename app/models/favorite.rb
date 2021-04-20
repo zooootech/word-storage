2b1a59ec723c4f72b7b53f5e8936fc8c929fca7d
zooootech <yzmisng@gmail.com>
2021-04-20 23:59:57 +0900

    ログイン中のユーザーのお気に入りワードの取得、並べ替えのコードをコメントアウト

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -8,21 +8,21 @@ class Favorite < ApplicationRecord
 
   # ユーザーのお気に入りにしてあるワードをアルファベット順に並べ替える処理
   # 引数にはuser_controller.rbにて、current_userが渡される
-  def self.favorite_words(user)
-    # 空の配列を作成
-    favorite_words = []
-    # 現在ログインしているユーザーのお気に入りを変数に代入
-    favorites = user.favorites
-    # 配列形式で格納されているお気に入り情報に対して繰り返し処理を行う
-    favorites.each do |favorite|
-      # 繰り返し処理の中で、お気に入り情報の内、ワード情報のみを取り出し、配列の要素として追加
-      favorite_words << favorite.word
-    end
-    # 配列にsort_byメソッドを用いて、昇順（今回の場合、アルファベット順）に並び替え
-    # 破壊的メソッド（!）を使用し、レシーバにあたるオブジェクト（今回の場合、@favorite_words）自身の値を変更
-    favorite_words.sort_by! do |a|
-      # アルファベット順に並べ替える際、同じアルファベットであれば小文字よりも大文字が上に来るよう記述
-      [a[:english].downcase, a[:english]]
-    end
-  end
+  # def self.favorite_words(user)
+  #   # 空の配列を作成
+  #   favorite_words = []
+  #   # 現在ログインしているユーザーのお気に入りを変数に代入
+  #   favorites = user.favorites
+  #   # 配列形式で格納されているお気に入り情報に対して繰り返し処理を行う
+  #   favorites.each do |favorite|
+  #     # 繰り返し処理の中で、お気に入り情報の内、ワード情報のみを取り出し、配列の要素として追加
+  #     favorite_words << favorite.word
+  #   end
+  #   # 配列にsort_byメソッドを用いて、昇順（今回の場合、アルファベット順）に並び替え
+  #   # 破壊的メソッド（!）を使用し、レシーバにあたるオブジェクト（今回の場合、@favorite_words）自身の値を変更
+  #   favorite_words.sort_by! do |a|
+  #     # アルファベット順に並べ替える際、同じアルファベットであれば小文字よりも大文字が上に来るよう記述
+  #     [a[:english].downcase, a[:english]]
+  #   end
+  # end
 end
